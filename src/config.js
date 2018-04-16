066a1b6327f1678e05db23f85a84083ab449b4ca
Kim Seungha <seungha.me@gmail.com>
2018-04-16 22:10:25 +0900

    설정 및 로직 초기화

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const GAME_ROOT = '#game-root';
-export const AUTO_START = false;
+export const AUTO_START = true;
 export const ROWS = 20;
 export const COLS = 30;
-export const INITIAL_DELAY = 300;
-export const DELAY_EXPONENT = 0.95;
+export const INITIAL_DELAY = 3000;
+export const DELAY_EXPONENT = 1;
